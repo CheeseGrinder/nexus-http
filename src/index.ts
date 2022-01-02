d1c400bc88d8c5b16fe093696cfbe4d0b1615c1f
Sukaato <sukaato.dev@gmail.com>
2022-01-02 14:21:17 +0100

    🐛 Fix export type
    - HttpHeaders
    - HttpHeadersInit
    - HttpResponse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './body';
 export { Client, FetchClient, XmlClient } from './client';
+export * from './headers';
 export * from './http';
 export * from './interceptors';
 export * from './status-code';
-export { HttpError, HttpMethod, Response, ResponseType } from './types';
+export { HttpError, HttpMethod, HttpResponse, Response, ResponseType } from './types';
